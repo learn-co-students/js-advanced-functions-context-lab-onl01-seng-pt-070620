17a3adc95ca8e70786b5176a7ff4d8245151f391
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-01-27 19:16:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,61 @@
-/* Your Code Here */
+let createEmployeeRecord = function(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+};
 
+let createEmployeeRecords = function(nestedArray) {
+    return nestedArray.map( emp => createEmployeeRecord(emp))
+};
+
+let createTimeInEvent = function(d) {
+    let newDate = d.split(" ")
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(newDate[1], 10),
+        date: newDate[0]
+    });
+    
+    return this
+};
+
+let createTimeOutEvent = function(d) {
+    let newDate = d.split(" ")
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(newDate[1], 10),
+        date: newDate[0]
+    })
+
+    return this
+};
+
+let hoursWorkedOnDate = function(d) {
+    let inTime = this.timeInEvents.find(function(onDate) {
+        return onDate.date === d
+    });
+    
+    let outTime = this.timeOutEvents.find(function(onDate) {
+        return onDate.date == d
+    });
+
+    return (outTime.hour - inTime.hour) / 100
+};
+
+let wagesEarnedOnDate = function(d) {
+    let hours = hoursWorkedOnDate.call(this, d)
+
+    let wage = this.payPerHour * hours
+
+    return wage
+};
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -19,4 +75,16 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
-}
+};
+
+let findEmployeeByFirstName = function(array, emp) {
+    return array.find(person => {
+        return person.firstName === emp
+    });
+};
+
+let calculatePayroll = function(array) {
+    return array.reduce(function(memo, emp) {
+        return memo + allWagesFor.call(emp)
+    }, 0)
+};
