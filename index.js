d38cbc7a399ce0846313b4e39cc27129a960574a
Srog1989 <s.rogers0715@yahoo.com>
2021-02-06 19:45:30 -0700

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,75 @@ let allWagesFor = function () {
 
     return payable
 }
+
+
+let createEmployeeRecord = function(column){
+    return {
+        firstName: column[0],
+        familyName: column[1],
+        title: column[2],
+        payPerHour: column[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(employeeInfo){
+    return employeeInfo.map(function(column){
+        return createEmployeeRecord(column)
+    })
+
+}
+
+const createTimeInEvent = function(dateTime){
+    let [date, hour] = dateTime.split(' ')
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+
+const createTimeOutEvent = function(dateTime){
+    let [date, hour] = dateTime.split(' ')
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+
+const hoursWorkedOnDate = function(day){
+    let timeIn = this.timeInEvents.find(function(e){
+        return e.date === day
+    })
+
+    let timeOut = this.timeOutEvents.find(function(e){
+        return e.date === day
+    })
+        return (timeOut.hour - timeIn.hour) / 100
+}
+
+const wagesEarnedOnDate= function( day){
+    return hoursWorkedOnDate.call(this, day) * this.payPerHour
+}
+
+const findEmployeeByFirstName = function(employees, name){
+    let employeeName  = employees.find(function(e){
+        return e.firstName === name
+    })
+    return employeeName
+
+}
+
+const calculatePayroll = function(employees){
+    let employeeWages = employees.map( e => allWagesFor.call(e))
+    let grandTotalOwed = employeeWages.reduce(function(sum, i){
+        return sum + i
+    })
+    return grandTotalOwed
+    }
